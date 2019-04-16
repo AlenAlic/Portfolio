6c632440493141ef608b488241d7558c2c15f5d3
Alen Alic <aalic89@gmail.com>
2019-04-16 14:59:33 +0200

    projects image fixes

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,9 +7,7 @@ const navItems = document.querySelectorAll(".nav-item");
 // Set Initial State Of Menu
 let showMenu = false;
 
-menuBtn.addEventListener("click", toggleMenu);
-
-function toggleMenu() {
+const toggleMenu = () => {
   if (!showMenu) {
     menuBtn.classList.add("close");
     menu.classList.add("show");
@@ -29,4 +27,31 @@ function toggleMenu() {
     // Set Menu State
     showMenu = false;
   }
-}
+};
+menuBtn.addEventListener("click", toggleMenu);
+
+// Modal
+const modalBtn = document.querySelectorAll(".modal-btn");
+
+const toggleModal = btn => {
+  let img = btn.target.currentSrc;
+  let modal = document.getElementById(btn.target.dataset.target);
+  let modalImg = modal.querySelector(".modal-content");
+  let modalCaption = modal.querySelector(".modal-caption");
+
+  modal.style.display = "block";
+  modalImg.src = img;
+  if (modalCaption !== null) {
+    modalCaption.innerHTML = btn.target.alt;
+  }
+
+  // Get the <span> element that closes the modal
+  var span = modal.querySelector(".modal-close");
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  };
+};
+
+modalBtn.forEach(ele => ele.addEventListener("click", toggleModal));
